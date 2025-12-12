69289e52ffd3a0c686c27cf812c291c5b4dce40f
Danil <danil.melnikov1404@gmail.com>
2025-12-12 19:49:17 +0500

    Add files via upload

---
+++ src/Lab4/Task7.java
@@ -0,0 +1,38 @@
+package Lab4;
+
+import java.util.Scanner;
+
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введите количество строк: ");
+        int rows = in.nextInt();
+
+        System.out.print("Введите количество столбцов: ");
+        int cols  = in.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        int z = 1;
+
+        for (int i = 0; i < rows; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < cols; j++) {
+                    matrix[i][j] = z++;
+                }
+            } else {
+                for (int j = cols - 1; j >= 0; j--) {
+                    matrix[i][j] = z++;
+                }
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.printf("%4d", matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
